013562a7c6731aca99cfc677c2b90cf43fe64bdf
Rohit Mourya <rohit.mourya@ilensys.com>
2026-04-14 12:45:33 +0530

    Installed React & TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h5>Hello my name is Rohit Mourya.</h5>
+    </>
+  )
+}
+
+export default App
